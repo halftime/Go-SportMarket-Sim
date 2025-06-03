41451a871af8367f082695b5d40ea2225e74245a
Ignc <60786122+halftime@users.noreply.github.com>
2025-06-03 23:08:18 +0200

    initial

---
+++ main/main.go
@@ -0,0 +1,86 @@
+package main
+
+// https://api.sportmarket.com/v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	// my packages
+	"main/myerrors"
+)
+
+func return_http_too_many_requests(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "API limit exceeded (429)", http.StatusTooManyRequests)
+}
+
+func return_http_not_found(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not Found (404)", http.StatusNotFound)
+	return
+}
+
+func return_http_not_authorized(w http.ResponseWriter, r *http.Request) {
+	notAuthReply := myerrors.ErrorReply{
+		Status: "error",
+		Code:   "auth_error",
+		Data: map[string]string{
+			"detail": "Authentication credentials were not provided.",
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusUnauthorized)
+	json.NewEncoder(w).Encode(notAuthReply)
+}
+
+func handler_get(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "This is a GET request handler. Use POST to submit data.")
+}
+
+func handler_authenticate(w http.ResponseWriter, r *http.Request) {
+	// Simulate authentication logic
+	// authHeader := r.Header.Get("Authorization")
+	// if authHeader == "" {
+	// 	return_http_not_authorized(w, r)
+	// 	return
+	// }
+
+	var data myerrors.LoginRequest
+	missingParameters := make(map[string][]string)
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		if data.Username == "" {
+			missingParameters["username"] = []string{"This field is required"}
+		}
+		if data.Password == "" {
+			missingParameters["password"] = []string{"This field is required"}
+		}
+	}
+
+	if len(missingParameters) > 0 {
+		notAuthReply := myerrors.ErrorReply{
+			Status: "error",
+			Code:   "validation_error",
+			Data:   missingParameters,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(notAuthReply)
+		return
+	}
+
+	// If authenticated, proceed with the request
+	fmt.Fprintln(w, "Authenticated successfully!")
+}
+
+func main() {
+	http.HandleFunc("/v1/sessions/", handler_authenticate) // Handle /v1/sessions/ with 429 Too Many Requests
+
+	http.HandleFunc("/v1", return_http_not_found)                  // Handle /v1 endpoint with 404 Not Found
+	http.HandleFunc("/v1/betslips", return_http_too_many_requests) // Handle /v1/betslips with custom Unauthorized
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ main/myerrors/myerrors.go
@@ -0,0 +1,28 @@
+package myerrors
+
+type ReplyEnvelope struct {
+	Status string `json:"status"`
+	Data   any    `json:"data"`
+}
+
+type ErrorReply struct {
+	Status string `json:"status"`
+	Code   string `json:"code"`
+	Data   any    `json:"data"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var Api_not_auth_reply = ErrorReply{
+	Status: "error",
+	Code:   "auth_error",
+	Data: map[string]string{
+		"detail": "Authentication credentials were not provided.",
+	},
+}
+
+// example for non authenticated request
+// {"status":"error","code":"auth_error","data":{"detail":"Authentication credentials were not provided."}}
